eb9a6dcbda3ceda706a3ac19a8b6aa7922749ea4
healer.kx.yu <healer.kx.yu@9048b9ba-60bd-11de-b3c1-0fd540ce45e4>
2013-02-07 04:03:16 +0000

    How to use UserControl and customize user's content.

---
+++ trunk/DAQ/Scada.Controls/ListViewPanel.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scada.Controls
+{
+	/// <summary>
+	/// Interaction logic for ListViewPanel.xaml
+	/// </summary>
+	public partial class ListViewPanel : UserControl
+	{
+		public ListViewPanel()
+		{
+			InitializeComponent();
+		}
+
+		public Button ListViewContent
+		{
+			get
+			{
+				return null;
+			}
+			set
+			{
+
+				this.ListViewContainer.Content = value;
+			}
+		}
+	}
+}
