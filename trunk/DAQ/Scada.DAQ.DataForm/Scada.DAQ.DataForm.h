d4a51c83a07b3da5e2612e66eb32a073078dfd18
healer.kx.yu <healer.kx.yu@9048b9ba-60bd-11de-b3c1-0fd540ce45e4>
2013-03-18 12:17:14 +0000

    ！

---
+++ trunk/DAQ/Scada.DAQ.DataForm/Scada.DAQ.DataForm.h
@@ -0,0 +1,32 @@
+
+// Scada.DAQ.DataForm.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CScadaDAQDataFormApp:
+// See Scada.DAQ.DataForm.cpp for the implementation of this class
+//
+
+class CScadaDAQDataFormApp : public CWinApp
+{
+public:
+	CScadaDAQDataFormApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CScadaDAQDataFormApp theApp;
