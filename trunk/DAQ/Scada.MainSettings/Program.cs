8f497617dc597dbd35ae719f35983dbfccc330ab
healer.kx.yu <healer.kx.yu@9048b9ba-60bd-11de-b3c1-0fd540ce45e4>
2013-04-20 14:50:35 +0000

    New Password and Settings.

---
+++ trunk/DAQ/Scada.MainSettings/Program.cs
@@ -0,0 +1,22 @@
+﻿using Scada.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Scada.MainSettings
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new SettingsForm());
+		}
+	}
+}
