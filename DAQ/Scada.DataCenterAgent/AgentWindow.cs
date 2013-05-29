823e23e426cb8d12c372f38b602b9db128d14a4f
healer.kx.yu <healer.kx.yu@9048b9ba-60bd-11de-b3c1-0fd540ce45e4>
2013-05-29 15:14:14 +0000

    ~~-

---
+++ DAQ/Scada.DataCenterAgent/AgentWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Scada.DataCenterAgent
+{
+    public partial class AgentWindow : Form
+    {
+        private Timer timer;
+
+        private Agent agent;
+
+        public AgentWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AgentWindow_Load(object sender, EventArgs e)
+        {
+            this.agent = this.CreateAgent("localhost", 2112);
+            this.InitializeTimer();
+        }
+
+        private Agent CreateAgent(string serverAddress, int serverPort)
+        {
+            Agent agent = new Agent();
+            agent.ServerAddress = serverAddress;
+            agent.ServerPort = serverPort;
+            agent.Greeting = "Hello";
+            agent.Connect();
+
+            return agent;
+        }
+
+        private void InitializeTimer()
+        {
+            this.timer = new Timer();
+            this.timer.Interval = 3000;
+            this.timer.Start();
+            this.timer.Tick += this.SendDataTick;
+        }
+
+        private void SendDataTick(object sender, EventArgs e)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes("Fucking\n");
+            this.agent.Send(bytes);
+        }
+    }
+}
---
+++ DAQ/Scada.DataCenterAgent/DataPacket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scada.DataCenterAgent
+{
+    public struct DataPacket
+    {
+        private StringBuilder sb;
+
+        private string deviceKey;
+
+
+        public DataPacket(string deviceKey)
+        {
+            this.deviceKey = deviceKey;
+            this.sb = new StringBuilder();
+        }
+
+        public void SetLength(int length)
+        {
+        }
+
+        public void Append()
+        {
+        }
+
+        public byte[] ToBytes()
+        {
+            return null;
+        }
+    }
+}
