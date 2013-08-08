f73b4720559e079b0876f6c15c5aea12c74e9439
healer.kx.yu <healer.kx.yu@9048b9ba-60bd-11de-b3c1-0fd540ce45e4>
2013-08-08 14:12:16 +0000

    !

---
+++ DAQ/Scada.Data.Client.Tcp/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scada.DataCenterAgent
+{
+    class Logger
+    {
+    }
+
+    class Log
+    {
+        public static Logger GetLogFile(string deviceKey)
+        {
+            return new Logger();
+        }
+    }
+}
